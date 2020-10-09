b3f9f88d7678a5a5736c1aab68a74add09109541
Adam Dadi <adamdadi23@gmail.com>
2020-10-09 22:20:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,5 @@
 def introduction(name)
-  puts "Hi, my name is #{name}"
-  puts "Hi, my name is #{name} and I am learning to program in #(language)."
+  puts "Hi, my name is #{name}."
+def introduction_with_language(name)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
